6b849b9709e1475e47efe93bffa0dacf0e68802a
AshGrey🥕 <ashgrey.huaier@gmail.com>
2025-06-06 00:18:11 +0800

    :sparkles: add `Boolean.Or` method

--- lib/Boolean/index.d.ts
+++ lib/Boolean/index.d.ts
@@ -11,7 +11,7 @@ declare namespace Boolean {
  * @example
  * type And1 = Boolean.And<true, false>;                            // false
  * type And2 = Boolean.And<Integer.Eq<Integer.Add<1, 2>, 3>, true>; // true
- * type And3 = Boolean.Adn<Integer.Lower<-3, -9>, false>;           // false
+ * type And3 = Boolean.And<Integer.Lower<-3, -9>, false>;           // false
  */
 export type And<A extends boolean, B extends boolean>
   = A extends true
@@ -20,5 +20,24 @@ export type And<A extends boolean, B extends boolean>
       : false
     : false;
 
+/**
+ * This is a boolean type function to return the value of expression
+ * like `a || b`.
+ * 
+ * @param A `a` in expression `a || b`.
+ * @param B `b` in expression `a || b`.
+ * @returns The result of expression `a || b` in JavaScript.
+ * 
+ * @example
+ * type Or1 = Boolean.Or<true, false>;                            // true
+ * type Or2 = Boolean.Or<Integer.Eq<Integer.Add<1, 2>, 3>, true>; // true
+ * type Or3 = Boolean.Or<Integer.Lower<-3, -9>, false>;           // false
+ */
+export type Or<A extends boolean, B extends boolean>
+  = A extends true
+    ? true
+    : B extends true
+      ? true
+      : false;
 
 }
