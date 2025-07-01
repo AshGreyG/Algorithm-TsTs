27dbdad2b4aab68f51d7bf2ea930f6fa9a65799e
AshGrey🥕 <ashgrey.huaier@gmail.com>
2025-07-01 09:28:28 +0800

    :test_tube: test(Array.LastIndexOf): add passed tests for `Array.LastIndexOf` method

--- test/lib-Array.test.ts
+++ test/lib-Array.test.ts
@@ -89,4 +89,10 @@ type CaseLibArray = [
   Expect<Equal<Array.At<[], -1>, never>>,
   Expect<Equal<Array.At<[], 0>, never>>,
 
+  // Array.LastIndexOf
+  Expect<Equal<Array.LastIndexOf<[1,2,1,2,1,3,4], 1>, 4>>,
+  Expect<Equal<Array.LastIndexOf<[], 1>, -1>>,
+  Expect<Equal<Array.LastIndexOf<[1,2,3], 0>, -1>>,
+  Expect<Equal<Array.LastIndexOf<[[],[[]],[],[]], [[]]>, 1>>,
+
 ]
